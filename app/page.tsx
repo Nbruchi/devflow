2101d130d528a4febb37138e53f36b2c86a6f7fd
Nbruchi <nkundabagenzibruce@gmail.com>
2026-01-09 21:09:11 +0200

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 const Page = () => {
-  return (
-    <main>
-      <div className="font-medium text-2xl">Welcome to Dev Overflow</div>
-    </main>
-  );
+    return (
+        <main>
+            <div className="text-2xl font-medium">Welcome to Dev Overflow</div>
+        </main>
+    );
 };
 
 export default Page;
