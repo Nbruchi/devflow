a12dcba2436a098b4d7ca0fcc0cd54cff11c9aa5
Nbruchi <nkundabagenzibruce@gmail.com>
2026-01-15 20:25:20 +0200

    setup mongodb and mongoose

---
+++ components/editor/index.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import {
+  BoldItalicUnderlineToggles,
+  ChangeCodeMirrorLanguage,
+  codeBlockPlugin,
+  codeMirrorPlugin,
+  ConditionalContents,
+  CreateLink,
+  diffSourcePlugin,
+  headingsPlugin,
+  imagePlugin,
+  InsertCodeBlock,
+  InsertImage,
+  InsertTable,
+  InsertThematicBreak,
+  linkDialogPlugin,
+  linkPlugin,
+  listsPlugin,
+  ListsToggle,
+  markdownShortcutPlugin,
+  MDXEditor,
+  MDXEditorMethods,
+  quotePlugin,
+  Separator,
+  tablePlugin,
+  thematicBreakPlugin,
+  toolbarPlugin,
+  UndoRedo,
+} from "@mdxeditor/editor";
+import { ForwardedRef } from "react";
+import { basicDark } from "cm6-theme-basic-dark";
+
+import "@mdxeditor/editor/style.css";
+import "./dark-editor.css";
+import { useTheme } from "next-themes";
+import { codeBlockLanguages } from "@/constants";
+
+interface Props {
+  value: string;
+  fieldChange: (value: string) => void;
+  editorRef: ForwardedRef<MDXEditorMethods> | null;
+}
+
+const Editor = ({ value, fieldChange, editorRef, ...props }: Props) => {
+  const { resolvedTheme } = useTheme();
+  const theme = resolvedTheme === "dark" ? [basicDark] : [];
+
+  return (
+    <MDXEditor
+      key={resolvedTheme}
+      markdown={value}
+      onChange={fieldChange}
+      ref={editorRef}
+      {...props}
+      className="background-light800_dark200 light-border-2 markdown-editor dark-editor w-full border grid"
+      plugins={[
+        headingsPlugin(),
+        listsPlugin(),
+        quotePlugin(),
+        linkPlugin(),
+        linkDialogPlugin(),
+        markdownShortcutPlugin(),
+        tablePlugin(),
+        imagePlugin(),
+        codeBlockPlugin({ defaultCodeBlockLanguage: "" }),
+        codeMirrorPlugin({
+          codeBlockLanguages: codeBlockLanguages,
+          autoLoadLanguageSupport: true,
+          codeMirrorExtensions: theme,
+        }),
+        diffSourcePlugin({
+          viewMode: "rich-text",
+          diffMarkdown: "",
+        }),
+        thematicBreakPlugin(),
+        markdownShortcutPlugin(),
+        toolbarPlugin({
+          toolbarContents: () => (
+            <ConditionalContents
+              options={[
+                {
+                  when: (editor) => editor?.editorType === "codeblock",
+                  contents: () => <ChangeCodeMirrorLanguage />,
+                },
+                {
+                  fallback: () => (
+                    <>
+                      <UndoRedo />
+                      <Separator />
+                      <BoldItalicUnderlineToggles />
+                      <Separator />
+                      <ListsToggle />
+                      <Separator />
+                      <CreateLink />
+                      <Separator />
+                      <InsertImage />
+                      <Separator />
+                      <InsertTable />
+                      <InsertThematicBreak />
+                      <InsertCodeBlock />
+                    </>
+                  ),
+                },
+              ]}
+            />
+          ),
+        }),
+      ]}
+    />
+  );
+};
+
+export default Editor;
--- constants/index.ts
+++ constants/index.ts
@@ -1,329 +1,377 @@
 export const sidebarLinks = [
-    {
-        imgURL: "/icons/home.svg",
-        route: "/",
-        label: "Home",
-    },
-    {
-        imgURL: "/icons/users.svg",
-        route: "/community",
-        label: "Community",
-    },
-    {
-        imgURL: "/icons/star.svg",
-        route: "/collection",
-        label: "Collections",
-    },
-    {
-        imgURL: "/icons/suitcase.svg",
-        route: "/jobs",
-        label: "Find Jobs",
-    },
-    {
-        imgURL: "/icons/tag.svg",
-        route: "/tags",
-        label: "Tags",
-    },
-    {
-        imgURL: "/icons/user.svg",
-        route: "/profile",
-        label: "Profile",
-    },
-    {
-        imgURL: "/icons/question.svg",
-        route: "/ask-question",
-        label: "Ask a question",
-    },
+  {
+    imgURL: "/icons/home.svg",
+    route: "/",
+    label: "Home",
+  },
+  {
+    imgURL: "/icons/users.svg",
+    route: "/community",
+    label: "Community",
+  },
+  {
+    imgURL: "/icons/star.svg",
+    route: "/collection",
+    label: "Collections",
+  },
+  {
+    imgURL: "/icons/suitcase.svg",
+    route: "/jobs",
+    label: "Find Jobs",
+  },
+  {
+    imgURL: "/icons/tag.svg",
+    route: "/tags",
+    label: "Tags",
+  },
+  {
+    imgURL: "/icons/user.svg",
+    route: "/profile",
+    label: "Profile",
+  },
+  {
+    imgURL: "/icons/question.svg",
+    route: "/ask-question",
+    label: "Ask a question",
+  },
 ];
 
 export const questions = [
-    {
-        _id: "1",
-        title: "How to learn React",
-        content: "I want to learn React. Can anyone suggest what to begin on?",
-        tags: [
-            { _id: "1", name: "React" },
-            { _id: "2", name: "JavaScript" },
-        ],
-        author: {
-            _id: "1",
-            name: "John Doe",
-            image: "https://c.pxhere.com/photos/e7/22/profile_face_person_man_human-1327211.jpg!d",
-        },
-        createdAt: new Date(),
-        upvotes: 0,
-        answers: 0,
-        views: 0,
+  {
+    _id: "1",
+    title: "How to learn React",
+    content: "I want to learn React. Can anyone suggest what to begin on?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: {
+      _id: "1",
+      name: "John Doe",
+      image: "https://c.pxhere.com/photos/e7/22/profile_face_person_man_human-1327211.jpg!d",
     },
-    {
-        _id: "2",
-        title: "How to learn TypeScript",
-        content: "I want to learn TypeScript. Can anyone suggest what to begin on?",
-        tags: [
-            { _id: "1", name: "TypeScript" },
-            { _id: "2", name: "JavaScript" },
-        ],
-        author: {
-            _id: "1",
-            name: "Gaylord Focker",
-            image: "https://www.goodfreephotos.com/cache/people/female-face-woman-portrait.jpg",
-        },
-        createdAt: new Date(),
-        upvotes: 0,
-        answers: 0,
-        views: 0,
+    createdAt: new Date(),
+    upvotes: 0,
+    answers: 0,
+    views: 0,
+  },
+  {
+    _id: "2",
+    title: "How to learn TypeScript",
+    content: "I want to learn TypeScript. Can anyone suggest what to begin on?",
+    tags: [
+      { _id: "1", name: "TypeScript" },
+      { _id: "2", name: "JavaScript" },
+    ],
+    author: {
+      _id: "1",
+      name: "Gaylord Focker",
+      image: "https://www.goodfreephotos.com/cache/people/female-face-woman-portrait.jpg",
     },
-    {
-        _id: "3",
-        title: "How to learn Next.js",
-        content: "I want to learn Next.js. Can anyone suggest what to begin on?",
-        tags: [
-            { _id: "1", name: "Next.js" },
-            { _id: "2", name: "React" },
-        ],
-        author: {
-            _id: "1",
-            name: "Miles Morales",
-            image: "https://p2.piqsels.com/preview/876/356/902/boy-businessman-close-up-eyes.jpg",
-        },
-        createdAt: new Date(),
-        upvotes: 0,
-        answers: 0,
-        views: 0,
+    createdAt: new Date(),
+    upvotes: 0,
+    answers: 0,
+    views: 0,
+  },
+  {
+    _id: "3",
+    title: "How to learn Next.js",
+    content: "I want to learn Next.js. Can anyone suggest what to begin on?",
+    tags: [
+      { _id: "1", name: "Next.js" },
+      { _id: "2", name: "React" },
+    ],
+    author: {
+      _id: "1",
+      name: "Miles Morales",
+      image: "https://p2.piqsels.com/preview/876/356/902/boy-businessman-close-up-eyes.jpg",
     },
-    {
-        _id: "4",
-        title: "How to learn Tailwind CSS",
-        content: "I want to learn Tailwind CSS. Can anyone suggest what to begin on?",
-        tags: [
-            { _id: "1", name: "Tailwind CSS" },
-            { _id: "2", name: "CSS" },
-        ],
-        author: {
-            _id: "1",
-            name: "Carl Russel",
-            image: "https://p2.piqsels.com/preview/941/886/553/analog-watch-businessman-casual-close-up.jpg",
-        },
-        createdAt: new Date(),
-        upvotes: 0,
-        answers: 0,
-        views: 0,
+    createdAt: new Date(),
+    upvotes: 0,
+    answers: 0,
+    views: 0,
+  },
+  {
+    _id: "4",
+    title: "How to learn Tailwind CSS",
+    content: "I want to learn Tailwind CSS. Can anyone suggest what to begin on?",
+    tags: [
+      { _id: "1", name: "Tailwind CSS" },
+      { _id: "2", name: "CSS" },
+    ],
+    author: {
+      _id: "1",
+      name: "Carl Russel",
+      image: "https://p2.piqsels.com/preview/941/886/553/analog-watch-businessman-casual-close-up.jpg",
     },
-    {
-        _id: "5",
-        title: "How to learn GraphQL",
-        content: "I want to learn GraphQL. Can anyone suggest what to begin on?",
-        tags: [
-            { _id: "1", name: "GraphQL" },
-            { _id: "2", name: "API" },
-        ],
-        author: {
-            _id: "1",
-            name: "Steve Doe",
-            image: "https://p0.piqsels.com/preview/760/89/310/adult-businessman-contemporary-corporate.jpg",
-        },
-        createdAt: new Date(),
-        upvotes: 0,
-        answers: 0,
-        views: 0,
+    createdAt: new Date(),
+    upvotes: 0,
+    answers: 0,
+    views: 0,
+  },
+  {
+    _id: "5",
+    title: "How to learn GraphQL",
+    content: "I want to learn GraphQL. Can anyone suggest what to begin on?",
+    tags: [
+      { _id: "1", name: "GraphQL" },
+      { _id: "2", name: "API" },
+    ],
+    author: {
+      _id: "1",
+      name: "Steve Doe",
+      image: "https://p0.piqsels.com/preview/760/89/310/adult-businessman-contemporary-corporate.jpg",
     },
+    createdAt: new Date(),
+    upvotes: 0,
+    answers: 0,
+    views: 0,
+  },
 ];
 
 export const techMap: { [key: string]: string } = {
-    // JavaScript variations
-    javascript: "devicon-javascript-plain",
-    js: "devicon-javascript-plain",
+  // JavaScript variations
+  javascript: "devicon-javascript-plain",
+  js: "devicon-javascript-plain",
 
-    // TypeScript variations
-    typescript: "devicon-typescript-plain",
-    ts: "devicon-typescript-plain",
+  // TypeScript variations
+  typescript: "devicon-typescript-plain",
+  ts: "devicon-typescript-plain",
 
-    // React variations
-    react: "devicon-react-original",
-    reactjs: "devicon-react-original",
+  // React variations
+  react: "devicon-react-original",
+  reactjs: "devicon-react-original",
 
-    // Next.js variations
-    nextjs: "devicon-nextjs-plain",
-    next: "devicon-nextjs-plain",
+  // Next.js variations
+  nextjs: "devicon-nextjs-plain",
+  next: "devicon-nextjs-plain",
 
-    // Node.js variations
-    nodejs: "devicon-nodejs-plain",
-    node: "devicon-nodejs-plain",
+  // Node.js variations
+  nodejs: "devicon-nodejs-plain",
+  node: "devicon-nodejs-plain",
 
-    // Bun.js variations
-    bun: "devicon-bun-plain",
-    bunjs: "devicon-bun-plain",
+  // Bun.js variations
+  bun: "devicon-bun-plain",
+  bunjs: "devicon-bun-plain",
 
-    // Deno.js variations
-    deno: "devicon-denojs-original",
-    denojs: "devicon-denojs-plain",
+  // Deno.js variations
+  deno: "devicon-denojs-original",
+  denojs: "devicon-denojs-plain",
 
-    // Python
-    python: "devicon-python-plain",
+  // Python
+  python: "devicon-python-plain",
 
-    // Java
-    java: "devicon-java-plain",
+  // Java
+  java: "devicon-java-plain",
 
-    // C++
-    "c++": "devicon-cplusplus-plain",
-    cpp: "devicon-cplusplus-plain",
+  // C++
+  "c++": "devicon-cplusplus-plain",
+  cpp: "devicon-cplusplus-plain",
 
-    // C#
-    "c#": "devicon-csharp-plain",
-    csharp: "devicon-csharp-plain",
+  // C#
+  "c#": "devicon-csharp-plain",
+  csharp: "devicon-csharp-plain",
 
-    // PHP
-    php: "devicon-php-plain",
+  // PHP
+  php: "devicon-php-plain",
 
-    // HTML
-    html: "devicon-html5-plain",
-    html5: "devicon-html5-plain",
+  // HTML
+  html: "devicon-html5-plain",
+  html5: "devicon-html5-plain",
 
-    // CSS
-    css: "devicon-css3-plain",
-    css3: "devicon-css3-plain",
+  // CSS
+  css: "devicon-css3-plain",
+  css3: "devicon-css3-plain",
 
-    // Git
-    git: "devicon-git-plain",
+  // Git
+  git: "devicon-git-plain",
 
-    // Docker
-    docker: "devicon-docker-plain",
+  // Docker
+  docker: "devicon-docker-plain",
 
-    // MongoDB
-    mongodb: "devicon-mongodb-plain",
-    mongo: "devicon-mongodb-plain",
+  // MongoDB
+  mongodb: "devicon-mongodb-plain",
+  mongo: "devicon-mongodb-plain",
 
-    // MySQL
-    mysql: "devicon-mysql-plain",
+  // MySQL
+  mysql: "devicon-mysql-plain",
 
-    // PostgreSQL
-    postgresql: "devicon-postgresql-plain",
-    postgres: "devicon-postgresql-plain",
+  // PostgreSQL
+  postgresql: "devicon-postgresql-plain",
+  postgres: "devicon-postgresql-plain",
 
-    // AWS variations
-    aws: "devicon-amazonwebservices-original",
-    "amazon web services": "devicon-amazonwebservices-original",
+  // AWS variations
+  aws: "devicon-amazonwebservices-original",
+  "amazon web services": "devicon-amazonwebservices-original",
 
-    // Firebase
-    firebase: "devicon-firebase-plain",
+  // Firebase
+  firebase: "devicon-firebase-plain",
 
-    // GraphQL
-    graphql: "devicon-graphql-plain",
+  // GraphQL
+  graphql: "devicon-graphql-plain",
 
-    // Vue.js
-    vue: "devicon-vuejs-plain",
-    vuejs: "devicon-vuejs-plain",
+  // Vue.js
+  vue: "devicon-vuejs-plain",
+  vuejs: "devicon-vuejs-plain",
 
-    // Tailwind CSS
-    tailwind: "devicon-tailwindcss-plain",
-    tailwindcss: "devicon-tailwindcss-plain",
+  // Tailwind CSS
+  tailwind: "devicon-tailwindcss-plain",
+  tailwindcss: "devicon-tailwindcss-plain",
 
-    // Angular
-    angular: "devicon-angularjs-plain",
+  // Angular
+  angular: "devicon-angularjs-plain",
 
-    // jQuery
-    jquery: "devicon-jquery-plain",
+  // jQuery
+  jquery: "devicon-jquery-plain",
 
-    // SASS
-    sass: "devicon-sass-plain",
+  // SASS
+  sass: "devicon-sass-plain",
 
-    // Laravel
-    laravel: "devicon-laravel-plain",
+  // Laravel
+  laravel: "devicon-laravel-plain",
 
-    // Ruby on Rails
-    rails: "devicon-rails-plain",
-    ruby: "devicon-rails-plain",
+  // Ruby on Rails
+  rails: "devicon-rails-plain",
+  ruby: "devicon-rails-plain",
 
-    // Redis
-    redis: "devicon-redis-plain",
+  // Redis
+  redis: "devicon-redis-plain",
 
-    // Linux
-    linux: "devicon-linux-plain",
+  // Linux
+  linux: "devicon-linux-plain",
 
-    // Jenkins
-    jenkins: "devicon-jenkins-plain",
+  // Jenkins
+  jenkins: "devicon-jenkins-plain",
 
-    // Kubernetes
-    kubernetes: "devicon-kubernetes-plain",
+  // Kubernetes
+  kubernetes: "devicon-kubernetes-plain",
 
-    // Azure
-    azure: "devicon-azure-plain",
-    "azure devops": "devicon-azure-plain",
+  // Azure
+  azure: "devicon-azure-plain",
+  "azure devops": "devicon-azure-plain",
 
-    // Google Cloud Platform
-    gcp: "devicon-googlecloud-plain",
-    "google cloud": "devicon-googlecloud-plain",
+  // Google Cloud Platform
+  gcp: "devicon-googlecloud-plain",
+  "google cloud": "devicon-googlecloud-plain",
 
-    // Bitbucket
-    bitbucket: "devicon-bitbucket-plain",
+  // Bitbucket
+  bitbucket: "devicon-bitbucket-plain",
 
-    // NGINX
-    nginx: "devicon-nginx-plain",
+  // NGINX
+  nginx: "devicon-nginx-plain",
 
-    // Blender
-    blender: "devicon-blender-plain",
+  // Blender
+  blender: "devicon-blender-plain",
 
-    // Unity
-    unity: "devicon-unity-original",
+  // Unity
+  unity: "devicon-unity-original",
 
-    // Unreal Engine
-    unreal: "devicon-unrealengine-original",
+  // Unreal Engine
+  unreal: "devicon-unrealengine-original",
 
-    // Swift
-    swift: "devicon-swift-plain",
+  // Swift
+  swift: "devicon-swift-plain",
 
-    // Kotlin
-    kotlin: "devicon-kotlin-plain",
+  // Kotlin
+  kotlin: "devicon-kotlin-plain",
 
-    // Go
-    go: "devicon-go-plain",
+  // Go
+  go: "devicon-go-plain",
 
-    // Rust
-    rust: "devicon-rust-plain",
+  // Rust
+  rust: "devicon-rust-plain",
 
-    // Dart
-    dart: "devicon-dart-plain",
+  // Dart
+  dart: "devicon-dart-plain",
 
-    // Flask
-    flask: "devicon-flask-original",
+  // Flask
+  flask: "devicon-flask-original",
 
-    // Django
-    django: "devicon-django-plain",
+  // Django
+  django: "devicon-django-plain",
 
-    // Bootstrap
-    bootstrap: "devicon-bootstrap-plain",
+  // Bootstrap
+  bootstrap: "devicon-bootstrap-plain",
 
-    // Material-UI
-    materialui: "devicon-materialui-plain",
-    "material ui": "devicon-materialui-plain",
+  // Material-UI
+  materialui: "devicon-materialui-plain",
+  "material ui": "devicon-materialui-plain",
 
-    // Adobe tools
-    photoshop: "devicon-photoshop-plain",
-    illustrator: "devicon-illustrator-plain",
-    xd: "devicon-xd-plain",
-    premiere: "devicon-premiere-plain",
+  // Adobe tools
+  photoshop: "devicon-photoshop-plain",
+  illustrator: "devicon-illustrator-plain",
+  xd: "devicon-xd-plain",
+  premiere: "devicon-premiere-plain",
 
-    // Figma
-    figma: "devicon-figma-plain",
+  // Figma
+  figma: "devicon-figma-plain",
 
-    // Inkscape
-    inkscape: "devicon-inkscape-plain",
+  // Inkscape
+  inkscape: "devicon-inkscape-plain",
 
-    // Trello
-    trello: "devicon-trello-plain",
+  // Trello
+  trello: "devicon-trello-plain",
 
-    // Jira
-    jira: "devicon-jira-plain",
+  // Jira
+  jira: "devicon-jira-plain",
 
-    // Heroku
-    heroku: "devicon-heroku-plain",
+  // Heroku
+  heroku: "devicon-heroku-plain",
 
-    // Elasticsearch
-    elasticsearch: "devicon-elasticsearch-plain",
+  // Elasticsearch
+  elasticsearch: "devicon-elasticsearch-plain",
 
-    // Redux
-    redux: "devicon-redux-original",
+  // Redux
+  redux: "devicon-redux-original",
 
-    // Three.js
-    threejs: "devicon-threejs-original",
+  // Three.js
+  threejs: "devicon-threejs-original",
+};
+
+export const codeBlockLanguages = {
+  js: "JavaScript",
+  ts: "TypeScript",
+  css: "CSS",
+  html: "HTML",
+  json: "JSON",
+  md: "Markdown",
+  xml: "XML",
+  yaml: "YAML",
+  python: "Python",
+  java: "Java",
+  c: "C",
+  cpp: "C++",
+  csharp: "C#",
+  go: "Go",
+  rust: "Rust",
+  swift: "Swift",
+  kotlin: "Kotlin",
+  ruby: "Ruby",
+  php: "PHP",
+  r: "R",
+  matlab: "MATLAB",
+  bash: "Bash",
+  powershell: "PowerShell",
+  sql: "SQL",
+  graphql: "GraphQL",
+  dockerfile: "Dockerfile",
+  dockercompose: "Docker Compose",
+  makefile: "Makefile",
+  toml: "TOML",
+  ini: "INI",
+  properties: "Properties",
+  java_properties: "Java Properties",
+  json5: "JSON5",
+  jsonc: "JSON with Comments",
+  jsonl: "JSON Lines",
+  csv: "CSV",
+  tsv: "TSV",
+  jsx: "JavaScript JSX",
+  tsx: "TypeScript JSX",
+  vue: "Vue",
+  svelte: "Svelte",
+  astro: "Astro",
+  mdx: "MDX",
+  markdown: "Markdown",
+  "": "Plain Text",
 };
