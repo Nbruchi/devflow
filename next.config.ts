fb65f02a87a960278ec44ff8684c1a57c7f38e49
Nbruchi <nkundabagenzibruce@gmail.com>
2026-01-16 20:03:45 +0200

    implement logging

--- next.config.ts
+++ next.config.ts
@@ -1,30 +1,31 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    reactCompiler: true,
-    experimental: {
-        turbopackFileSystemCacheForDev: true,
-    },
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "c.pxhere.com",
-            },
-            {
-                protocol: "https",
-                hostname: "www.goodfreephotos.com",
-            },
-            {
-                protocol: "https",
-                hostname: "p2.piqsels.com",
-            },
-            {
-                protocol: "https",
-                hostname: "p0.piqsels.com",
-            },
-        ],
-    },
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "c.pxhere.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.goodfreephotos.com",
+      },
+      {
+        protocol: "https",
+        hostname: "p2.piqsels.com",
+      },
+      {
+        protocol: "https",
+        hostname: "p0.piqsels.com",
+      },
+    ],
+  },
+  serverExternalPackages: ["pino", "pino-pretty"],
 };
 
 export default nextConfig;
